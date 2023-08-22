1cfdfe9cfe892843809761268a34e1f5a682e18e
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2023-08-23 00:54:04 +0700

    update src

--- employeeservice/src/main/java/com/vinhdien/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/src/main/java/com/vinhdien/employeeservice/command/controller/EmployeeCommandController.java
@@ -2,8 +2,11 @@ package com.vinhdien.employeeservice.command.controller;
 
 import java.util.UUID;
 
+import javax.xml.transform.Source;
+
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.MessageChannel;
 
 import com.vinhdien.employeeservice.command.command.CreateEmployeeCommand;
 import com.vinhdien.employeeservice.command.command.DeleteEmployeeCommand;
@@ -19,10 +23,14 @@ import com.vinhdien.employeeservice.command.model.EmployeeRequestModel;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@EnableBinding(Source.class)
 public class EmployeeCommandController {
 
     @Autowired
     private CommandGateway commandGateway;
+    
+	@Autowired
+	private MessageChannel output;
 
     @PostMapping
     public String addEmployee(@RequestBody EmployeeRequestModel model) {
@@ -46,4 +54,17 @@ public class EmployeeCommandController {
         commandGateway.sendAndWait(command);
         return "employee deleted";
     }
+    
+//	@PostMapping("/sendMessage")
+//	public void SendMessage(@RequestBody String message) {
+//		try {
+//			
+//			ObjectMapper mapper = new ObjectMapper();
+//			String json = mapper.writeValueAsString(message);
+//			output.send(MessageBuilder.withPayload(json).build());
+//		} catch (JsonProcessingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
