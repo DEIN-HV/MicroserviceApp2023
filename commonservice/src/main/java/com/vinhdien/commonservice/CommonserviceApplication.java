45502da37e139987bf9222de22c00eaeba016758
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2023-09-04 17:49:50 +0700

    add common service

---
+++ commonservice/src/main/java/com/vinhdien/commonservice/CommonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vinhdien.commonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonserviceApplication.class, args);
+	}
+
+}
