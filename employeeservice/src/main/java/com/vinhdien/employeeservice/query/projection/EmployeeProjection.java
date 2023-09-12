db0e366831dada9c66b0df36f9fd9d62a6fb9e88
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2023-09-13 00:07:44 +0700

    update src

--- employeeservice/src/main/java/com/vinhdien/employeeservice/query/projection/EmployeeProjection.java
+++ employeeservice/src/main/java/com/vinhdien/employeeservice/query/projection/EmployeeProjection.java
@@ -8,6 +8,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.vinhdien.commonservice.model.EmployeeResponseCommonModel;
+import com.vinhdien.commonservice.query.GetDetailsEmployeeQuery;
 import com.vinhdien.employeeservice.command.data.Employee;
 import com.vinhdien.employeeservice.command.data.EmployeeRepository;
 import com.vinhdien.employeeservice.query.model.EmployeeReponseModel;
@@ -17,36 +19,36 @@ import com.vinhdien.employeeservice.query.queries.GetEmployeesQuery;
 @Component
 public class EmployeeProjection {
 
-    @Autowired
-    EmployeeRepository employeeRepository;
+	@Autowired
+	EmployeeRepository employeeRepository;
 
-    @QueryHandler
-    public EmployeeReponseModel handle(GetEmployeesQuery getEmployeesQuery) {
-        EmployeeReponseModel model = new EmployeeReponseModel();
-        Employee employee = employeeRepository.getById(getEmployeesQuery.getEmployeeId());
-        BeanUtils.copyProperties(employee, model);
+	@QueryHandler
+	public EmployeeReponseModel handle(GetEmployeesQuery getEmployeesQuery) {
+		EmployeeReponseModel model = new EmployeeReponseModel();
+		Employee employee = employeeRepository.getById(getEmployeesQuery.getEmployeeId());
+		BeanUtils.copyProperties(employee, model);
 
-        return model;
-    }
+		return model;
+	}
 
-    @QueryHandler
-    public List<EmployeeReponseModel> handle(GetAllEmployeeQuery getallAllEmployeeQuery) {
-        List<EmployeeReponseModel> modelList = new ArrayList<>();
-        List<Employee> list = employeeRepository.findAll();
-        list.forEach(s -> {
-            EmployeeReponseModel model = new EmployeeReponseModel();
-            BeanUtils.copyProperties(s, model);
-            modelList.add(model);
-        });
-        return modelList;
-    }
+	@QueryHandler
+	public List<EmployeeReponseModel> handle(GetAllEmployeeQuery getallAllEmployeeQuery) {
+		List<EmployeeReponseModel> modelList = new ArrayList<>();
+		List<Employee> list = employeeRepository.findAll();
+		list.forEach(s -> {
+			EmployeeReponseModel model = new EmployeeReponseModel();
+			BeanUtils.copyProperties(s, model);
+			modelList.add(model);
+		});
+		return modelList;
+	}
 
-    // @QueryHandler
-    // public EmployeeResponseCommonModel handle(GetDetailsEmployeeQuery getDetailsEmployeeQuery) {
-	// 	EmployeeResponseCommonModel model = new EmployeeResponseCommonModel();
-	//  Employee employee = employeeRepository.getById(getDetailsEmployeeQuery.getEmployeeId());
-    //   BeanUtils.copyProperties(employee, model);
+	@QueryHandler
+	public EmployeeResponseCommonModel handle(GetDetailsEmployeeQuery getDetailsEmployeeQuery) {
+		EmployeeResponseCommonModel model = new EmployeeResponseCommonModel();
+		Employee employee = employeeRepository.getById(getDetailsEmployeeQuery.getEmployeeId());
+		BeanUtils.copyProperties(employee, model);
 
-    //     return model;
-    // }
+		return model;
+	}
 }
