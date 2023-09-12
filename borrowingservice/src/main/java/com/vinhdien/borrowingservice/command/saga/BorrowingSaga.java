440370b0830dd84954cb21df4aa0a35d0b16ab78
dien.hv <dien.hv@vn.vinx.asia>
2023-09-12 17:18:39 +0700

    update saga

--- borrowingservice/src/main/java/com/vinhdien/borrowingservice/command/saga/BorrowingSaga.java
+++ borrowingservice/src/main/java/com/vinhdien/borrowingservice/command/saga/BorrowingSaga.java
@@ -10,10 +10,15 @@ import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vinhdien.borrowingservice.command.command.DeleteBorrowCommand;
+import com.vinhdien.borrowingservice.command.command.SendMessageCommand;
 import com.vinhdien.borrowingservice.command.event.BorrowCreatedEvent;
+import com.vinhdien.borrowingservice.command.model.EmployeeResponseCommonModel;
+import com.vinhdien.commonservice.command.RollBackStatusBookCommand;
 import com.vinhdien.commonservice.command.UpdateStatusBookCommand;
+import com.vinhdien.commonservice.event.BookUpdateStatusEvent;
 import com.vinhdien.commonservice.model.BookResponseCommonModel;
 import com.vinhdien.commonservice.query.GetDetailsBookQuery;
+import com.vinhdien.commonservice.query.GetDetailsEmployeeQuery;
 
 @Saga
 public class BorrowingSaga {
@@ -50,6 +55,35 @@ public class BorrowingSaga {
 
 			System.out.println(e.getMessage());
 		}
+	}
+
+	@SagaEventHandler(associationProperty = "bookId")
+	private void handle(BookUpdateStatusEvent event) {
+		System.out.println("BookUpdateStatusEvent in Saga for BookId : " + event.getBookId());
+		try {
+			GetDetailsEmployeeQuery getDetailsEmployeeQuery = new GetDetailsEmployeeQuery(event.getEmployeeId());
+
+			EmployeeResponseCommonModel employeeResponseCommonModel = queryGateway.query(getDetailsEmployeeQuery,
+					ResponseTypes.instanceOf(EmployeeResponseCommonModel.class))
+					.join();
+			if (employeeResponseCommonModel.getIsDisciplined() == true) {
+				throw new Exception("Nhan vien bi ky luat");
+			} else {
+				commandGateway.sendAndWait(new SendMessageCommand(event.getBorrowId(), event.getEmployeeId(),
+						"Da muon sach thanh cong !"));
+				SagaLifecycle.end();
+			}
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+			rollBackBookStatus(event.getBookId(), event.getEmployeeId(), event.getBorrowId());
+		}
+	}
+
+	private void rollBackBookStatus(String bookId, String employeeId, String borrowId) {
+		SagaLifecycle.associateWith("bookId", bookId);
+		RollBackStatusBookCommand command = new RollBackStatusBookCommand(bookId, true, employeeId, borrowId);
+		commandGateway.sendAndWait(command);
 	}
 
 	private void rollBackBorrowRecord(String id) {
